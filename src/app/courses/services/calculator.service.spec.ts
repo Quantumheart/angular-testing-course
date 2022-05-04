72b83992b4390be51eeb84bd72884723d6b855ed
Cody Mumford <cody-mumford9106@hotmail.com>
2022-05-04 16:02:47 -0500

    add calculator.service.spec.ts; update signatures in calculator.service.ts

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,17 @@
+﻿import {CalculatorService} from './calculator.service';
+import {LoggerService} from './logger.service';
+
+
+describe('CalculatorService', () => {
+   it('adds two numbers', () => {
+     const calculator = new CalculatorService(new LoggerService());
+     const result = calculator.add(1, 1);
+      expect(result).toBe(2);
+   });
+
+  it('subtracts two numbers', () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(1, 1);
+    expect(result).toBe(0, 'unexpected subtraction result');
+  });
+});
