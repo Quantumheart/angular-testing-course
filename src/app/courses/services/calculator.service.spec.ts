12cc6dac4dc58bd23abea1183f7acaec97dcb721
Cody Mumford <cody-mumford9106@hotmail.com>
2022-05-04 16:19:54 -0500

    implement spy in adds two numbers

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -4,9 +4,11 @@ import {LoggerService} from './logger.service';
 
 describe('CalculatorService', () => {
    it('adds two numbers', () => {
-     const calculator = new CalculatorService(new LoggerService());
+     const logger = jasmine.createSpyObj('LoggerService', ['log']);
+     const calculator = new CalculatorService(logger);
      const result = calculator.add(1, 1);
       expect(result).toBe(2);
+      expect(logger.log).toHaveBeenCalledTimes(2);
    });
 
   it('subtracts two numbers', () => {
